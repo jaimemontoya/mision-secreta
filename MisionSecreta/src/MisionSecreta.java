4182f363531773f96f787961acc697eb15d2ce1b
Jaime Montoya <webmaster@jaimemontoya.com>
2021-05-05 19:26:51 -0600

    Crear métodos para cada sistema de encriptación.

--- MisionSecreta/src/MisionSecreta.java
+++ MisionSecreta/src/MisionSecreta.java
@@ -1,9 +1,18 @@
 public class MisionSecreta {
 	public static void main(String[] args) {
 		MisionSecreta ms = new MisionSecreta();
-		ms.codificar("ayuda");
+		System.out.println(ms.codificar("ayuda"));
 	}
-	public void codificar(String mensaje) {
-		System.out.println("Mensaje: "+mensaje);
+	public String codificar(String mensaje) {
+		return morse(mensaje) + octal(mensaje) + bacedifogu(mensaje);
+	}
+	public String morse(String mensaje) {
+		return "> " + mensaje + " (Morse)\n\n";
+	}
+	public String octal(String mensaje) {
+		return "> " + mensaje + " (Octal)\n\n";
+	}
+	public String bacedifogu(String mensaje) {
+		return "> " + mensaje + " (BACEDIFOGU)\n\n";
 	}
 }
